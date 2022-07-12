cbdc7dc9771066252acda93bd2869c3362bee237
Brian Pham <hungpham0506@gmail.com>
2022-07-12 21:16:37 +0700

    Implement solution problem2_v2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
     pluck(:status, :city).count{|user| user.status == status && user.city == city} > 0
   end
 
+  # Based on the schema in https://gist.github.com/ancv1990/0187a1fecad17e11331565b73db93042
+  # This method was slow because column `user_id` in the table `posts` does not index.
+  # We should add an index to the column in which we using it where the clause
   def self.bad_sol
     User.select("*, (select count(*) from posts where posts.user_id=users.id) as post_count")
     .order("post_count desc")
